ce525f770f67a4f9713f48aa395e2abcde2d0819
xmx <xmxisme@gmail.com>
2023-12-10 17:16:24 +0800

    test

---
+++ src/main/java/com/xmxisme/App.java
@@ -0,0 +1,13 @@
+package com.xmxisme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
